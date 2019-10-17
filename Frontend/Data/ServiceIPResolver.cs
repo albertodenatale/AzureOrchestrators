cf8ce47ac6e432d263adbe49631c94d15e3c72bb
Alberto De Natale <alberto.de.natale@avanade.com>
2019-10-17 08:40:09 +0200

    implemented reverse proxy

---
+++ Frontend/Data/ServiceIPResolver.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Fabric;
+
+namespace Frontend.Data
+{
+    public interface IServiceIPResolver
+    {
+        Uri GetServiceUri(string serviceName);
+        Uri GetServiceName(string serviceName);
+    }
+
+    public class ServiceIPResolver: IServiceIPResolver
+    {
+        private readonly StatelessServiceContext context;
+        private readonly string reverseProxyBaseUri;
+
+        public ServiceIPResolver(IConfiguration configuration, StatelessServiceContext context)
+        {
+            this.context = context;
+            this.reverseProxyBaseUri = configuration["Configuration:ReverseProxyUri"];
+        }
+
+        /// <summary>
+        /// Constructs a service name for a specific poll.
+        /// Example: fabric:/VotingApplication/polls/name-of-poll
+        /// </summary>
+        /// <param name="poll"></param>
+        /// <returns></returns>
+        public Uri GetServiceUri(string serviceName)
+        {
+            var uri = GetServiceName(serviceName);
+
+            return GetProxyAddress(uri);
+        }
+
+        public Uri GetServiceName(string serviceName)
+        {
+            return new Uri($"{context.CodePackageActivationContext.ApplicationName}/{serviceName}");
+        }
+
+        /// <summary>
+        /// Constructs a reverse proxy URL for a given service.
+        /// Example: http://localhost:19081/VotingApplication/VotingData/
+        /// </summary>
+        /// <param name="serviceName"></param>
+        /// <returns></returns>
+        private Uri GetProxyAddress(Uri serviceName)
+        {
+            return new Uri($"{this.reverseProxyBaseUri}{serviceName.AbsolutePath}");
+        }
+    }
+}
--- Frontend/Data/WeatherForecastService.cs
+++ Frontend/Data/WeatherForecastService.cs
@@ -1,24 +1,42 @@
 using Flurl.Http;
-using Microsoft.Extensions.Configuration;
 using Models;
 using System;
+using System.Fabric;
+using System.Fabric.Query;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Frontend.Data
 {
-    public class WeatherForecastService 
-    {    
-        private string apiUrl;
+    public interface IWeatherForecastService
+    {
+        Task<WeatherForecast[]> GetForecastAsync(DateTime startDate);
+    }
 
-        public WeatherForecastService(IConfiguration configuration)
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly FabricClient fabricClient;
+        private readonly IServiceIPResolver serviceIpResolver;
+
+        public WeatherForecastService(IServiceIPResolver serviceIPResolver, FabricClient fabricClient)
         {
-            this.apiUrl = configuration["Configuration:BackendUrl"];
+            this.fabricClient = fabricClient;
+            this.serviceIpResolver = serviceIPResolver;
         }
 
         public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
-            return await $"{apiUrl}/weather".GetJsonAsync<WeatherForecast[]>();
+            var serviceName = serviceIpResolver.GetServiceName("Backend");
+            var serviceUri = serviceIpResolver.GetServiceUri("Backend");
+
+            Partition partition = (await this.fabricClient.QueryManager.GetPartitionListAsync(serviceName)).FirstOrDefault();
+
+            if (partition != null)
+            {
+                return await $"{serviceUri}/api/Weather?PartitionKey={((Int64RangePartitionInformation)partition.PartitionInformation).LowKey}&PartitionKind=Int64Range".GetJsonAsync<WeatherForecast[]>();
+            }
+
+            return null;
         }
     }
 }
